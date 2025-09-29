f398cbb9fd0396d091fe8904d47d799f7c57186f
ThetSweLin <122963030+ThetSweLin@users.noreply.github.com>
2025-09-29 16:36:56 +0700

    ..

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
-import path from "path"
+import path from "path";
 
 dotenv.config();
 
@@ -24,10 +24,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.use("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 
